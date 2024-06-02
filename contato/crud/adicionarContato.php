5eef666ebd4f084cd5f5b144dd6fbf08f79b2cef
Andrei <andreifrancap@gmail.com>
2024-06-02 15:00:10 -0300

    add: validacao de email cadastrado

--- contato/crud/adicionarContato.php
+++ contato/crud/adicionarContato.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-    <div class="container">
+    <div class="container delete-operation">
         <div class="row">
             <?php 
                 require "../../conectaDb.php";
@@ -17,6 +17,16 @@
                 $tipo = $_POST["tipo"];
                 $empresa = $_POST["empresa"];
                 $email = $_POST["email"];
+                
+                $email_existente = $banco->query("SELECT * FROM contato WHERE email = '$email'");
+                if ($email_existente->num_rows > 0) {
+                    include "../navbarContato.php";
+                    echo "<body class='bg-danger'>";
+                    echo "<h2 class='text-center mt-5'>Erro ao cadastrar contato</h2>";
+                    echo "<h3 class='text-center mt-2'>Email já cadastrado</h3>";
+                    echo "</body>";
+                    exit;
+                }
                 if (empty($nome) || empty($telefone) || empty($tipo)) {
                     echo "Preencha nome, telefone e tipo";
                 } else {
